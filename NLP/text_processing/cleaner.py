43e8786a14fbca61b6d7083c20f33f4e16a63f99
Abhishek <abhi08as.as@gmail.com>
2021-06-14 14:56:23 +0530

    Clean

--- NLP/text_processing/cleaner.py
+++ NLP/text_processing/cleaner.py
@@ -1,5 +1,6 @@
 import string
 from typing import List
+import re
 
 
 def clean_corpus(corpus) -> List[str]:
@@ -17,9 +18,14 @@ def clean_corpus(corpus) -> List[str]:
 
     sen_list = []
     for sentence in corpus:
-        sentence = sentence.translate(str.maketrans(
-            "", "", string.punctuation)).translate(str.maketrans("", "", string.digits))
+        sentence = sentence.translate(
+            str.maketrans("", "", string.punctuation))
 
+        sentence = sentence.translate(str.maketrans("", "", string.digits))
+
+        sentence = sentence.replace(",", "")
+
+        sentence = sentence.replace("n\'t", " not")
         sentence = sentence.strip()
 
         sen_list.append(sentence)
@@ -27,3 +33,11 @@ def clean_corpus(corpus) -> List[str]:
     sen_list = list(filter(lambda x: x != '', sen_list))
 
     return sen_list
+
+
+if __name__ == "__main__":
+    corpus = """
+    Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then
+    """
+
+    print(clean_corpus(corpus))
--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from streamlit.elements.utils import clean_text
 from scikit_learn.NLP.feature_extraction.Tfidf import Tfidf
 from NLP.feature_extraction.BOW import BagOfWords
 from NLP.text_processing.cleaner import clean_corpus
@@ -10,7 +9,7 @@ st.markdown("<h1 style='text-align: center;'>Text feature extractor</h1>",
             unsafe_allow_html=True)
 
 option = st.selectbox(
-    "Select feature extractor",
+    "Select feature extract or",
     ("TFIDF", "Bag of Words"),
 )
 
@@ -23,34 +22,38 @@ if option == "Bag of Words":
 corpus = st.text_area('Enter paragraph', '',
                       help='Enter your paragraph here')
 
-if not corpus:
-    st.write("Enter some text!")
-else:
-    cleaned_corpus = clean_corpus(corpus=corpus)
+if st.button("Calculate"):
 
-    if option == "TFIDF":
-
-        tfidf = Tfidf(cleaned_corpus)
-
-        unq = tfidf.unique_words()
-
-        vec = tfidf.compute_tfidf().toarray()
-
+    if not corpus:
+        st.write("Enter some text!")
     else:
+        cleaned_corpus = clean_corpus(corpus=corpus)
 
-        bow = BagOfWords(cleaned_corpus)
+        st.write(cleaned_corpus)
 
-        vec = bow.make_BoW(binary=True if type_ == "Binary" else False)
+        if option == "TFIDF":
 
-        unq = bow.unique_words()
+            tfidf = Tfidf(cleaned_corpus)
 
-    df = pd.DataFrame(vec, columns=unq)
+            unq = tfidf.unique_words()
 
-    st.dataframe(data=df)
+            vec = tfidf.compute_tfidf().toarray()
 
-    csv = df.to_csv(index=False)
+        else:
 
-    # From: https://discuss.streamlit.io/t/how-to-add-a-download-excel-csv-function-to-a-button/4474
-    b64 = base64.b64encode(csv.encode()).decode()
-    linko = f'<center><a href="data:file/csv;base64,{b64}" download="myfilename.csv">Download as CSV file</a></center>'
-    st.markdown(linko, unsafe_allow_html=True)
+            bow = BagOfWords(cleaned_corpus)
+
+            vec = bow.make_BoW(binary=True if type_ == "Binary" else False)
+
+            unq = bow.unique_words()
+
+        df = pd.DataFrame(vec, columns=unq)
+
+        st.dataframe(data=df)
+
+        csv = df.to_csv(index=False)
+
+        # From: https://discuss.streamlit.io/t/how-to-add-a-download-excel-csv-function-to-a-button/4474
+        b64 = base64.b64encode(csv.encode()).decode()
+        linko = f'<center><a href="data:file/csv;base64,{b64}" download="myfilename.csv">Download as CSV file</a></center>'
+        st.markdown(linko, unsafe_allow_html=True)
