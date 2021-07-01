cc6665b7ca7dd3e1def87d2e669054b567c727b1
Abhishek <abhi08as.as@gmail.com>
2021-07-01 22:51:24 +0530

    clean

--- Cpp/Preprocess/src/Preprocess.cpp
+++ Cpp/Preprocess/src/Preprocess.cpp
@@ -6,14 +6,14 @@ std::map<std::string, std::vector<int>> Preprocess::one_hot_encode()
 
     int i = 0;
 
-    for (auto label : this->labels)
-    {
-        std::vector<int> vec(this->labels.size(), 0);
+    std::for_each(this->labels.begin(), this->labels.end(), [&](std::string label) -> void
+                  {
+                      std::vector<int> vec(this->labels.size(), 0);
 
-        vec[i++] = 1;
+                      vec[i++] = 1;
 
-        encoded_dict[label] = vec;
-    }
+                      encoded_dict[label] = vec;
+                  });
 
     return encoded_dict;
 }
